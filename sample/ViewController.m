13d496311995f3796bd7350408ad9c98b4cf2dd7
vanbungkring <prasetyo.mailbox@gmail.com>
2016-10-13 14:22:56 +0700

    Initial Commit

---
+++ sample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  sample
+//
+//  Created by Arie on 10/13/16.
+//  Copyright © 2016 Arie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
